0b903b25d98f862cbf88025b0a47cac17927946b
Johan Rodriguez <josrodriguezmo@unal.edu.co>
2020-06-14 15:38:49 -0500

    solución reto 2 password

--- src/main.py
+++ src/main.py
@@ -1,11 +1,51 @@
 # Resolve the problem!!
 import string
-
+import random 
 SYMBOLS = list('!"#$%&\'()*+,-./:;?@[]^_`{|}~')
+letters_lower = list(string.ascii_lowercase)
+letters_upper = list(string.ascii_uppercase)
 
 
+    
 def generate_password():
-    # Start coding here
+    password=''
+    letras_pass = []
+    # hagamos la elección de la longitud del password
+    len_password = random.randint(8,16)
+    for i in range(len_password):
+        sym = random.choice(SYMBOLS)
+        lower = random.choice(letters_lower)
+        upper=random.choice(letters_upper)
+        num= str(random.randint(0,10))
+        choice = random.choice(sym + lower+ upper +num)
+        letras_pass.append(choice)
+    password = password.join(letras_pass)
+
+    add_upper = random.choice(list(password))
+    idx_upper = password.index(add_upper)
+    password = password.replace(add_upper,random.choice(letters_upper))
+    add_lower =random.choice(list(password))
+    idx_lower = password.index(add_lower)
+    while idx_lower==idx_upper:
+        add_lower = random.choice(list(password))
+        idx_lower = password.index(add_lower) 
+    password = password.replace(add_lower,random.choice(letters_lower))
+    add_sym = random.choice(list(password))
+    idx_sym= password.index(add_sym)
+    while idx_sym==idx_lower or idx_sym==idx_upper:
+        add_sym = random.choice(list(password))
+        idx_sym= password.index(add_sym)
+    password = password.replace(add_sym,random.choice(SYMBOLS))
+    add_num = random.choice(list(password))  
+    idx_num = password.index(add_num)
+    while idx_num in (idx_lower,idx_sym,idx_upper):
+        add_num = random.choice(list(password))  
+        idx_num = password.index(add_num)
+    password = password.replace(add_num,str(random.randint(0,10)))
+
+
+    return password
+    
 
 
 def validate(password):
@@ -43,6 +83,7 @@ def validate(password):
 
 def run():
     password = generate_password()
+    print(password)
     if validate(password):
         print('Secure Password')
     else:
